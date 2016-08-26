5c824f32170b455c31e63e498fcc1c73366ab30e
joypingping <joypingping2013@163.com>
2016-08-26 18:58:05 +0800

    冒泡排序

---
+++ 01冒泡排序.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01冒泡排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class BubbleSort
+    {
+        public int[] bubbleSort(int[] A,int n)
+        {
+            for(int i=0;i<A.Length-1;i++)
+            {
+                for(int j=0;j<A.Length-i-1;j++)
+                {
+                    if(A[j]>A[j+1])
+                    {
+                        int temp = A[j];
+                        A[j] = A[j + 1];
+                        A[j + 1] = temp;
+                    }
+                }
+            }
+
+            return A;
+        }
+    }
+}
